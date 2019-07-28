798052f493ddcd9f73ee5fd31b9f157cd436e085
赵文祺 <1027572886a@gmail.com>
2019-07-29 00:00:14 +0800

    add wsgi

--- backend/app.py
+++ backend/app.py
@@ -9,15 +9,17 @@ from resources.user_resource import api as ns_user
 from resources.security import api as ns_security
 from resources.model import api as ns_ml
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 # choose a env {'dev', 'test', 'prod'}
 APP_ENV = getenv('APP_ENV', 'dev')
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.from_object(config[APP_ENV])
 
 api = Api(app, version='1.0', title='User Demo Api', prefix='/api')
 db.init_app(app)
 jwt = JWTManager(app)
-
 
 api.add_namespace(ns_user)
 api.add_namespace(ns_security)
