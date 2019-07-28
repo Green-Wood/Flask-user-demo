c3de08e8d3fcd3716d649199d3232ee2520ea0c3
赵文祺 <1027572886a@gmail.com>
2019-07-28 10:50:26 +0800

    add deploy rsa

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -1,4 +1,5 @@
 import unittest
+from flask import json
 from app import app
 
 
@@ -8,10 +9,14 @@ class TestUserMethod(unittest.TestCase):
     def setUp(self) -> None:
         self.app = app.test_client()
 
-    def test_get(self):
-        response = self.app.post('/model/predict', data={
-            'instances': [1, 2, 3]
-        })
+    def test_model(self):
+        response = self.app.post(
+            '/api/model/predict',
+            json={
+                'instances': [1, 2, 3]
+            }
+        )
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {
             'predictions': [2.5, 3, 3.5]
         })
